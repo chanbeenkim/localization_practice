003b71b1348dda291031c8a3f0457137b43fe20d
beenfilm94@gmail.com <kcb9582@naver.com>
2024-01-11 22:49:02 +0900

    feat: Localizations 한/영 구현
    easy localizations 패키지를 통해 localization을 구현했다. 언어는 Json파일로 구성했고 버튼을 통해 언어 스위치 또한 구현했다. flutter localizations 패키지 보다 훨씬 간소화 되어 있는 느낌이다. (일어 추가 예정)

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,42 +1,72 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _isChanged = false;
+  void onKrPressed() {
+    setState(() {
+      EasyLocalization.of(context)!.setLocale(
+        const Locale('ko'),
+      );
+      _isChanged = false;
+    });
+  }
+
+  void onEnPressed() {
+    setState(() {
+      EasyLocalization.of(context)!.setLocale(
+        const Locale('en'),
+      );
+      _isChanged = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Localization"),
+        backgroundColor: Colors.teal[400],
+        elevation: 0,
+        title: const Text('title').tr(),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "안녕하세요",
-                style: TextStyle(
-                  fontSize: 40,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Container(
-            color: Colors.blue,
-            child: TextButton(
-              onPressed: () {},
-              child: const Text(
-                "Click",
-                style: TextStyle(color: Colors.white),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              tr('hello'),
+              style: const TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.w600,
               ),
             ),
-          )
-        ],
+            const SizedBox(
+              height: 20,
+            ),
+            OutlinedButton.icon(
+              onPressed: () {
+                _isChanged ? onKrPressed() : onEnPressed();
+              },
+              icon: const Icon(
+                Icons.language_outlined,
+                color: Colors.black,
+              ),
+              label: Text(
+                _isChanged ? 'korean'.tr() : 'english'.tr(),
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,32 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:localization_practice/l10n/l10n.dart';
 
 import 'home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+final supportedLocales = [
+  const Locale('en'),
+  const Locale('ko'),
+];
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(EasyLocalization(
+      supportedLocales: supportedLocales,
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en'),
+      child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      supportedLocales: L10n.all,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       home: const HomeScreen(),
     );
   }
